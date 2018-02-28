1955256988729b3d2bd38897f67fc5fba78b955f
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2018-02-28 08:55:23 +0100

    add classes samuelgauthier/bsc-th#2

---
+++ source/renderer/Water.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_WATER
+#define INCLUDED_WATER
+
+//#include "lib/ogl.h"
+
+class Water
+{
+public:
+    
+    
+public:
+    Water();
+    ~Water();
+    
+};
+
+#endif // INCLUDED_WATER
