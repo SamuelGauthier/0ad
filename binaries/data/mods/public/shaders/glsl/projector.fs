165ad6be54e9a013842d7fd508bacde9f34ac2db
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2018-04-05 15:41:46 +0200

    fix water map boundaries
    SamuelGauthier/bsc-th#9

--- binaries/data/mods/public/shaders/glsl/projector.fs
+++ binaries/data/mods/public/shaders/glsl/projector.fs
@@ -1,6 +1,17 @@
 #version 120
 
+uniform sampler2D losMap;
+
+varying vec2 losCoords;
+
 void main()
 {
-	gl_FragColor = vec4(1, 0, 0, 1);
+	float losMod;
+
+
+	losMod = texture2D(losMap, losCoords.st).a;
+	losMod = losMod < 0.03 ? 0.0 : losMod;
+
+	vec4 color = vec4(1, 0, 0, 1);
+	gl_FragColor = color * losMod;
 }
